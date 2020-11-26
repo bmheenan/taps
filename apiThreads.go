0a95db7341ec55a0fc80c08d43eb6b77c8b2e75a
bmheenan <bmheenan@users.noreply.github.com>
2020-11-26 14:42:31 -0500

    - Thread ids: int -> int64

--- apiThreads.go
+++ apiThreads.go
@@ -2,15 +2,15 @@ package taps
 
 // APIThreadsPostReq defines the request for a POST to /threads
 type APIThreadsPostReq struct {
-	Name  string `json:"name"`
-	Owner string `json:"owner"`
-	Iter  string `json:"iter"`
-	Cost  int    `json:"cost"`
-	Pas   []int  `json:"parents"`
-	Chs   []int  `json:"children"`
+	Name  string  `json:"name"`
+	Owner string  `json:"owner"`
+	Iter  string  `json:"iter"`
+	Cost  int     `json:"cost"`
+	Pas   []int64 `json:"parents"`
+	Chs   []int64 `json:"children"`
 }
 
 // APIThreadsPostRes defines the response for a POST to /threads
 type APIThreadsPostRes struct {
-	ID int `json:"id"`
+	ID int64 `json:"id"`
 }
